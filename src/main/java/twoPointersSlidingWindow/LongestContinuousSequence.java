ff12ce7c916960ccc41818337bf988a1332cd5fe
keeeeeey <sseioul@naver.com>
2022-07-31 12:02:54 +0900

    최대 길이 부분 수열 구하기

---
+++ src/main/java/twoPointersSlidingWindow/LongestContinuousSequence.java
@@ -0,0 +1,34 @@
+package twoPointersSlidingWindow;
+
+import java.util.Scanner;
+
+public class LongestContinuousSequence {
+    public int solution(int n, int k, int[] arr) {
+        int answer = 0, cnt = 0, lt = 0;
+        for (int rt = 0; rt < n; rt++) {
+            if (arr[rt] == 0) {
+                cnt++;
+            }
+            while (cnt > k) {
+                if (arr[lt] == 0) {
+                    cnt--;
+                }
+                lt++;
+            }
+            answer = Math.max(answer, rt - lt + 1);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        LongestContinuousSequence lcs = new LongestContinuousSequence();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        System.out.println(lcs.solution(n, k, arr));
+    }
+}
