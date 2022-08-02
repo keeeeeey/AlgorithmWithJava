0d8dd7d6d7a202fb2d240fd110d3416e6bfcfb54
keeeeeey <sseioul@naver.com>
2022-08-02 20:39:24 +0900

    LRU

---
+++ src/main/java/sortingAndSearching/LeastRecentlyUsed.java
@@ -0,0 +1,43 @@
+package sortingAndSearching;
+
+import java.util.Scanner;
+
+public class LeastRecentlyUsed {
+    public int[] solution(int s, int n, int[] arr) {
+        int[] answer = new int[s];
+        for (int x : arr) {
+            int pos = -1;
+            for (int i = 0; i < s; i++) {
+                if (arr[i] == x) {
+                    pos = i;
+                }
+            }
+            if (pos == -1) {
+                for (int i = s - 1; i >= 1; i--) {
+                    answer[i] = answer[i - 1];
+                }
+                answer[0] = x;
+            } else {
+                for (int i = pos; i >= 1; i--) {
+                    answer[i] = answer[i - 1];
+                }
+                answer[0] = x;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        LeastRecentlyUsed lru = new LeastRecentlyUsed();
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt();
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int x : lru.solution(s, n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
