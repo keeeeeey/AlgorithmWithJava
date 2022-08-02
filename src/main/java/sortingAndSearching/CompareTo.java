0138f3774ab25c7e89924441f8998baaad9ed450
keeeeeey <sseioul@naver.com>
2022-08-02 21:54:54 +0900

    좌표정렬

---
+++ src/main/java/sortingAndSearching/CompareTo.java
@@ -0,0 +1,40 @@
+package sortingAndSearching;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+class Point implements Comparable<Point> {
+    public int x, y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point point) {
+        if (this.x == point.x) {
+            return this.y - point.y;
+        } else {
+            return this.x - point.x;
+        }
+    }
+}
+
+public class CompareTo {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        ArrayList<Point> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int x = kb.nextInt();
+            int y = kb.nextInt();
+            arr.add(new Point(x, y));
+        }
+        Collections.sort(arr);
+        for (Point point : arr) {
+            System.out.println(point.x + " " + point.y);
+        }
+    }
+}
