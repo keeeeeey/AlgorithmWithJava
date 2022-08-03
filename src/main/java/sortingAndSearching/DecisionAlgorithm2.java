e6030dad906fd6d5236a46ee8736fe6632aca49e
keeeeeey <sseioul@naver.com>
2022-08-03 22:31:57 +0900

    마구간 정하기(결정알고리즘)

---
+++ src/main/java/sortingAndSearching/DecisionAlgorithm2.java
@@ -0,0 +1,48 @@
+package sortingAndSearching;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DecisionAlgorithm2 {
+    public int count(int[] arr, int dist) {
+        int cnt = 1, ep = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - ep >= dist) {
+                ep = arr[i];
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+
+    public int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        Arrays.sort(arr);
+        int lt = 1;
+        int rt = arr[n - 1];
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (count(arr, mid) >= m) {
+                answer = mid;
+                lt = mid + 1;
+            } else {
+                rt = mid - 1;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        DecisionAlgorithm2 da = new DecisionAlgorithm2();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(da.solution(n, m, arr));
+    }
+}
