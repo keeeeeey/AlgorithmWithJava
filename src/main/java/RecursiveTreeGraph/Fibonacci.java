701e293d373905fb5d92c9a0c0523c303c19f22f
keeeeeey <sseioul@naver.com>
2022-08-08 20:25:17 +0900

    피보나치 재귀

--- src/main/java/RecursiveTreeGraph/Fibonacci.java
+++ src/main/java/RecursiveTreeGraph/Fibonacci.java
@@ -1,4 +1,28 @@
 package RecursiveTreeGraph;
 
 public class Fibonacci {
+    static int[] fibo;
+    public int dfs(int n) {
+        if (fibo[n] > 0) {
+            return fibo[n];
+        }
+
+        if (n == 1) {
+            return fibo[n] = 1;
+        } else if (n == 2) {
+            return fibo[n] = 1;
+        } else {
+            return fibo[n] = dfs(n - 2) + dfs(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Fibonacci fibonacci = new Fibonacci();
+        int n = 10;
+        fibo = new int[n + 1];
+        fibonacci.dfs(n);
+        for (int i = 1; i <= n; i++) {
+            System.out.print(fibo[i] + " ");
+        }
+    }
 }
