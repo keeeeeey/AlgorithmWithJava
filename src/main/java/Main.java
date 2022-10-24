3bee0aab0f8ad1dc2c87df5680298aed09f8e903
keeeeeey <sseioul@naver.com>
2022-10-24 23:34:42 +0900

    자바 알고리즘 문제 풀이

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public int solution(String str) {
-        String original = "";
-        for (int i = 0; i < str.length(); i++) {
-            if (!Character.isAlphabetic(str.charAt(i))) {
-                original += str.charAt(i);
+    public int solution(int[] arr, int n) {
+        int answer = 1;
+        int maxH = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > maxH) {
+                answer += 1;
+                maxH = arr[i];
             }
         }
-        int answer = Integer.parseInt(original);
         return answer;
     }
 
     public static void main(String[] args) throws IOException {
         Main m = new Main();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        System.out.println(m.solution(str));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(m.solution(arr, n));
+//        for (int num : m.solution(arr, n)) {
+//            System.out.print(num + " ");
+//        }
     }
 
 }
