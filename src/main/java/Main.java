7a7b294ceb0495ff01854f2f34953aea806c604e
keeeeeey <sseioul@naver.com>
2022-12-02 14:49:29 +0900

    리팩토링 테스트

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
-
 public class Main {
+    public static int splitAndSum(String text) {
+        if (isEmpty(text)) return 0;
 
-    public int solution(int[] arr, int n) {
-        int answer = 1;
-        int maxH = arr[0];
-        for (int i = 1; i < n; i++) {
-            if (arr[i] > maxH) {
-                answer += 1;
-                maxH = arr[i];
-            }
-        }
-        return answer;
+        return getSum(toInts(text.split("-")));
     }
 
-    public static void main(String[] args) throws IOException {
-        Main m = new Main();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+    public static int[] toInts(String[] values) {
+        int[] numbers = new int[values.length];
+
+        for (int i = 0; i < values.length; i++) {
+            numbers[i] = Integer.parseInt(values[i]);
         }
-        System.out.println(m.solution(arr, n));
-//        for (int num : m.solution(arr, n)) {
-//            System.out.print(num + " ");
-//        }
+
+        return numbers;
     }
 
+    public static int getSum(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+
+    public static boolean isEmpty(String text) {
+        if (text == null) return true;
+        return text.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+        int ret = splitAndSum("11-22-33");
+
+        System.out.println(ret);
+
+    }
 }
