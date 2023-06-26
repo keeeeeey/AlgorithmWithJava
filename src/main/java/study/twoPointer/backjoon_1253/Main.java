1ad208612cc971bf45c25f454d9061885da33a6f
keeeeeey <sseioul@naver.com>
2023-06-26 14:41:13 +0900

    백준 투포인터, 슬라이딩 윈도우

---
+++ src/main/java/study/twoPointer/backjoon_1253/Main.java
@@ -0,0 +1,59 @@
+package study.twoPointer.backjoon_1253;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int N;
+
+    public static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(solution());
+    }
+
+    private static int solution() {
+        int answer = 0;
+
+        for (int i = 0; i < N; i++) {
+            int left = 0;
+            int right = N - 1;
+
+            while (left < right) {
+                if (left == i) {
+                    left += 1;
+                    continue;
+                } else if (right == i) {
+                    right -= 1;
+                    continue;
+                }
+
+                if (arr[left] + arr[right] > arr[i]) {
+                    right -= 1;
+                } else if (arr[left] + arr[right] < arr[i]) {
+                    left += 1;
+                } else {
+                    answer += 1;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
---
+++ src/main/java/study/twoPointer/backjoon_1593/Main.java
@@ -0,0 +1,72 @@
+package study.twoPointer.backjoon_1593;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int g, s;
+
+    public static String W, S;
+
+    public static HashMap<Character, Integer> mapW = new HashMap<>();
+
+    public static HashMap<Character, Integer> check = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        g = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
+        W = br.readLine();
+        S = br.readLine();
+
+        // cAda mapW c: 1, A : 1, d : 1, a : 1
+        // AbrAcadAbRa
+        // check c : 0 , A : 0,
+        for (int i = 0; i < g; i++) {
+            mapW.put(W.charAt(i), mapW.getOrDefault(W.charAt(i), 0) + 1);
+            check.put(W.charAt(i), 0);
+        }
+
+        System.out.println(solution());
+    }
+
+    private static int solution() {
+        int answer = 0;
+        int cnt = 0;
+
+        for (int i = 0; i < s; i++) {
+            if (i >= g) {
+                if (check.containsKey(S.charAt(i - g))) {
+                    cnt -= 1;
+                    check.put(S.charAt(i - g), check.get(S.charAt(i - g)) - 1);
+                }
+            }
+
+            if (check.containsKey(S.charAt(i))) {
+                cnt += 1;
+                check.put(S.charAt(i), check.get(S.charAt(i)) + 1);
+            }
+
+            if (cnt == g) {
+                boolean flag = true;
+                for (char c : check.keySet()) {
+                    if (check.get(c) != mapW.get(c)) {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag) {
+                    answer += 1;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
