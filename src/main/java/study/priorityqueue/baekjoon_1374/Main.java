ac61e98485ad9f060b77c5814b2430c829f3b354
keeeeeey <sseioul@naver.com>
2023-08-11 09:58:33 +0900

    강의실 배정

---
+++ src/main/java/study/priorityqueue/baekjoon_1374/Main.java
@@ -0,0 +1,63 @@
+package study.priorityqueue.baekjoon_1374;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static class Time {
+        int id;
+        int start;
+        int end;
+
+        public Time(int id, int start, int end) {
+            this.id = id;
+            this.start = start;
+            this.end = end;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Time[] arr = new Time[N];
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int id = Integer.parseInt(st.nextToken());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            arr[i] = new Time(id, start, end);
+        }
+        Arrays.sort(arr, new Comparator<Time>() {
+            @Override
+            public int compare(Time o1, Time o2) {
+                return o1.start - o2.start;
+            }
+        });
+
+        PriorityQueue<Time> pq = new PriorityQueue<>(new Comparator<Time>() {
+            @Override
+            public int compare(Time o1, Time o2) {
+                return o1.end - o2.end;
+            }
+        });
+        pq.offer(arr[0]);
+
+        int answer = 1;
+        for (int i = 1; i < N; i++) {
+            if (arr[i].start < pq.peek().end) {
+                answer += 1;
+            } else {
+                pq.poll();
+            }
+            pq.offer(arr[i]);
+        }
+        System.out.println(answer);
+    }
+}
