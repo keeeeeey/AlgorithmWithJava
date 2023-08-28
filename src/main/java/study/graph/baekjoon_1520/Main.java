fcdb80383b5f3af18c7bf7e709b1a2505281cea8
keeeeeey <sseioul@naver.com>
2023-08-28 10:33:52 +0900

    백준 그래프 탐색 dp 내리막길

---
+++ src/main/java/study/graph/baekjoon_1520/Main.java
@@ -0,0 +1,62 @@
+package study.graph.baekjoon_1520;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static class Node {
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static int N, M;
+    public static int[][] arr, dp;
+    public static int[] dx = {1, -1, 0, 0};
+    public static int[] dy = {0, 0, 1, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        arr = new int[M][N];
+        dp = new int[M][N];
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = -1;
+            }
+        }
+        System.out.println(solution(0, 0));
+    }
+
+    private static int solution(int x, int y) {
+        if (x == M - 1 && y == N - 1) {
+            return 1;
+        }
+
+        if (dp[x][y] != -1) {
+            return dp[x][y];
+        }
+
+        dp[x][y] = 0;
+        for (int d = 0; d < 4; d++) {
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (nx >= 0 && nx < M && ny >= 0 && ny < N && arr[nx][ny] < arr[x][y]) {
+                dp[x][y] += solution(nx, ny);
+            }
+        }
+        return dp[x][y];
+    }
+
+}
