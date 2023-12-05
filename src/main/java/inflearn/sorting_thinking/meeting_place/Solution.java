25ee1be46b55903a3d4038b965a94f778fc81e55
keeeeeey <sseioul@naver.com>
2023-12-05 12:15:45 +0900

    모임장소

---
+++ src/main/java/inflearn/sorting_thinking/meeting_place/Solution.java
@@ -0,0 +1,34 @@
+package inflearn.sorting_thinking.meeting_place;
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] board){
+        int answer=0;
+        int n = board.length;
+        List<Integer> row = new ArrayList<>();
+        List<Integer> col = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 1) {
+                    row.add(i);
+                    col.add(j);
+                }
+            }
+        }
+        col.sort(Comparator.naturalOrder());
+        int x = row.get(row.size() / 2);
+        int y = col.get(col.size() / 2);
+        for (int i = 0; i < row.size(); i++) {
+            answer = answer + Math.abs(row.get(i) - x) + Math.abs(col.get(i) - y);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Solution T = new Solution();
+        System.out.println(T.solution(new int[][]{{1, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 1}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}}));
+        System.out.println(T.solution(new int[][]{{1, 0, 0, 0, 1}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 1, 0}}));
+        System.out.println(T.solution(new int[][]{{1, 0, 0, 0, 1, 1}, {0, 1, 0, 0, 1, 0}, {0, 1, 0, 0, 0, 0}, {0, 0, 0, 0, 1, 0}, {0, 0, 0, 0, 0, 1}, {1, 0, 0, 0, 1, 1}}));
+    }
+}
